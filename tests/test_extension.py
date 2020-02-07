2812a8652b983eddd90434abad45a40a8b0ca718
Anton Lodder <alodder@opusonesolutions.com>
2020-02-07 09:34:41 -0500

    Refactor tests for readible output

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -1,4 +1,5 @@
 import markdown
+from .assertions import assert_xml_equal
 
 
 def test_docstring():
@@ -9,13 +10,33 @@ def test_docstring():
     :docstring:
 """
     output = markdown.markdown(content, extensions=["mkautodoc"])
-    assert output.splitlines() == [
-        "<h1>Example</h1>",
-        '<div class="autodoc">',
-        '<div class="autodoc-signature"><code>mocklib.<strong>example_function</strong></code><span class="autodoc-punctuation">(</span><em class="autodoc-param">a</em><span class="autodoc-punctuation">, </span><em class="autodoc-param">b=None</em><span class="autodoc-punctuation">, </span><em class="autodoc-param">*args</em><span class="autodoc-punctuation">, </span><em class="autodoc-param">**kwargs</em><span class="autodoc-punctuation">)</span></div>',
-        '<div class="autodoc-docstring"><p>This is a function with a <em>docstring</em>.</p></div>',
-        "</div>",
-    ]
+    assert_xml_equal(
+        output,
+        """\
+<h1>Example</h1>
+<div class="autodoc">
+  <div class="autodoc-signature">
+    <code>
+      mocklib.
+      <strong>example_function</strong>
+    </code>
+    <span class="autodoc-punctuation">(</span>
+    <em class="autodoc-param">a</em>
+    <span class="autodoc-punctuation">, </span>
+    <em class="autodoc-param">b=None</em>
+    <span class="autodoc-punctuation">, </span>
+    <em class="autodoc-param">*args</em>
+    <span class="autodoc-punctuation">, </span>
+    <em class="autodoc-param">**kwargs</em>
+    <span class="autodoc-punctuation">)</span>
+  </div>
+  <div class="autodoc-docstring">
+    <p>
+      This is a function with a <em>docstring</em>.
+    </p>
+  </div>
+</div>""",
+    )
 
 
 def test_async_function():
@@ -23,11 +44,21 @@ def test_async_function():
 ::: mocklib.example_async_function
 """
     output = markdown.markdown(content, extensions=["mkautodoc"])
-    assert output.splitlines() == [
-        '<div class="autodoc">',
-        '<div class="autodoc-signature"><em>async </em><code>mocklib.<strong>example_async_function</strong></code><span class="autodoc-punctuation">(</span><span class="autodoc-punctuation">)</span></div>',
-        "</div>",
-    ]
+    assert_xml_equal(
+        output,
+        """\
+<div class="autodoc">
+  <div class="autodoc-signature">
+    <em>async </em>
+    <code>
+      mocklib.
+      <strong>example_async_function</strong>
+    </code>
+    <span class="autodoc-punctuation">(</span>
+    <span class="autodoc-punctuation">)</span>
+  </div>
+</div>""",
+    )
 
 
 def test_members():
@@ -39,16 +70,53 @@ def test_members():
     :members:
 """
     output = markdown.markdown(content, extensions=["mkautodoc"])
-    assert output.splitlines() == [
-        "<h1>Example</h1>",
-        '<div class="autodoc">',
-        '<div class="autodoc-signature"><em>class </em><code>mocklib.<strong>ExampleClass</strong></code><span class="autodoc-punctuation">(</span><span class="autodoc-punctuation">)</span></div>',
-        '<div class="autodoc-docstring"><p>This is a class with a <em>docstring</em>.</p></div>',
-        '<div class="autodoc-members">',
-        '<div class="autodoc-signature"><code><strong>example_method</strong></code><span class="autodoc-punctuation">(</span><em class="autodoc-param">self</em><span class="autodoc-punctuation">, </span><em class="autodoc-param">a</em><span class="autodoc-punctuation">, </span><em class="autodoc-param">b=None</em><span class="autodoc-punctuation">)</span></div>',
-        '<div class="autodoc-docstring"><p>This is a method with a <em>docstring</em>.</p></div>',
-        '<div class="autodoc-signature"><code><strong>example_property</strong></code></div>',
-        '<div class="autodoc-docstring"><p>This is a property with a <em>docstring</em>.</p></div>',
-        "</div>",
-        "</div>",
-    ]
+    assert_xml_equal(
+        output,
+        """\
+<h1>Example</h1>
+<div class="autodoc">
+  <div class="autodoc-signature">
+    <em>class </em>
+    <code>
+      mocklib.
+      <strong>ExampleClass</strong>
+    </code>
+    <span class="autodoc-punctuation">(</span>
+    <span class="autodoc-punctuation">)</span>
+  </div>
+  <div class="autodoc-docstring">
+    <p>
+      This is a class with a <em>docstring</em>.
+    </p>
+  </div>
+  <div class="autodoc-members">
+    <div class="autodoc-signature">
+      <code>
+        <strong>example_method</strong>
+      </code>
+      <span class="autodoc-punctuation">(</span>
+      <em class="autodoc-param">self</em>
+      <span class="autodoc-punctuation">, </span>
+      <em class="autodoc-param">a</em>
+      <span class="autodoc-punctuation">, </span>
+      <em class="autodoc-param">b=None</em>
+      <span class="autodoc-punctuation">)</span>
+    </div>
+    <div class="autodoc-docstring">
+      <p>
+        This is a method with a <em>docstring</em>.
+      </p>
+    </div>
+    <div class="autodoc-signature">
+      <code>
+        <strong>example_property</strong>
+      </code>
+    </div>
+    <div class="autodoc-docstring">
+      <p>
+        This is a property with a <em>docstring</em>.
+      </p>
+    </div>
+  </div>
+</div>""",
+    )
