24853c19f0afc6836d9cf6be1ff9210f111454e8
Anton Lodder <alodder@opusonesolutions.com>
2020-02-20 09:42:35 -0500

    Put the most important assertion function at the top

--- tests/assertions.py
+++ tests/assertions.py
@@ -3,6 +3,40 @@ from xml.dom import minidom
 import textwrap
 
 
+def assert_xml_equal(xml_string, expected_xml_string):
+    """
+    Assert equality of two xml strings, particularly that the contents of
+    each string have the same elements, with the same attributes (e.g. class,
+    text) and the same non-xml string contents
+    """
+    # this prints a human-formatted string of what the test passed in -- useful
+    # if you need to modify test expectations after you've modified
+    # a rendering and tested it visually
+    print(to_readable_error_output(xml_string))
+
+    assert_elements_equal(
+        etree.ElementTree.fromstring(tostring(xml_string)),
+        etree.ElementTree.fromstring(tostring(expected_xml_string)),
+    )
+
+
+def assert_elements_equal(element, reference_element):
+    """
+    Assert, recursively, the equality of two etree objects.
+    """
+    assert (
+        element.text == reference_element.text
+    ), f"Text doesn't match: {element.text} =/= {reference_element.text}."
+    assert (
+        element.attrib == reference_element.attrib
+    ), f"Attrib doesn't match: {element.attrib} =/= {reference_element.attrib}"
+    assert len(element) == len(
+        reference_element
+    ), f"Expected {len(reference_element)} children but got {len(element)}"
+    for sub_element, reference_sub_element in zip(element, reference_element):
+        assert_elements_equal(sub_element, reference_sub_element)
+
+
 def tostring(xml_string):
     """
     Wraps `xml_string` in a div so it can be rendered, even if it has multiple roots.
@@ -35,32 +69,3 @@ def remove_indents(html):
     """
     lines = [el.lstrip() for el in html.split("\n")]
     return "".join([el for el in lines if el or el != "\n"])
-
-
-def assert_elements_equal(element, reference_element):
-    """
-    Assert, recursively, the equality of two etree objects.
-    """
-    assert (
-        element.text == reference_element.text
-    ), f"Text doesn't match: {element.text} =/= {reference_element.text}."
-    assert (
-        element.attrib == reference_element.attrib
-    ), f"Attrib doesn't match: {element.attrib} =/= {reference_element.attrib}"
-    assert len(element) == len(
-        reference_element
-    ), f"Expected {len(reference_element)} children but got {len(element)}"
-    for sub_element, reference_sub_element in zip(element, reference_element):
-        assert_elements_equal(sub_element, reference_sub_element)
-
-
-def assert_xml_equal(xml_string, expected_xml_string):
-    # this prints a human-formatted string of what the test passed in -- useful
-    # if you need to modify test expectations after you've modified
-    # a rendering and tested it visually
-    print(to_readable_error_output(xml_string))
-
-    assert_elements_equal(
-        etree.ElementTree.fromstring(tostring(xml_string)),
-        etree.ElementTree.fromstring(tostring(expected_xml_string)),
-    )
